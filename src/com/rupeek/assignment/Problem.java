15617285cc1c708b8bf88dec1025625477fe4700
kumarrahul9292 <kumarrahul9292@gmail.com>
2019-06-22 13:15:35 +0530

    first commit

---
+++ src/com/rupeek/assignment/Problem.java
@@ -0,0 +1,12 @@
+package com.rupeek.assignment;
+
+public class Problem {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		sol.findPairs(1002);
+//		boolean val = sol.validateB(512, 28121);
+//		System.out.println(val);
+	}
+
+}
