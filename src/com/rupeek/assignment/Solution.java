441161b819faa6dc439a0abade5f531a08cc31d5
kumarrahul9292 <kumarrahul9292@gmail.com>
2019-06-23 03:44:57 +0530

    bug fix

--- src/com/rupeek/assignment/Solution.java
+++ src/com/rupeek/assignment/Solution.java
@@ -6,58 +6,79 @@ public class Solution {
 		if (N<10) {
 			System.out.println("No Pairs exist.");
 		}
-		for (int i=10; i<=N; i++) {
+		for (int i=10; i<N; i++) {
 			int B = N-i;
-//			if (B<10) {
-//				B = String.valueOf(B);
-//			}
-			if(validateB(B, i) && (countDigit(B) == (countDigit(i)-1) || countDigit(B) == 1)) {
+			
+			if(validateB(B, i) && ((countDigit(B) == (countDigit(i)-1) || countDigit(B) == 1))) {
+//			if(validateB(B, i) && (countDigit(B) == (countDigit(i)-1))) {	
 				System.out.println("Pairs: "+ i + " + " + B + " = " + N);
 			}
 		}
 	}
-	
-	
-//			906 + 96 = 1002
-//			911 + 91 = 1002
-//			951 + 51 = 1002
-
-//			991 + 11 = 1002	
-//			903 + 99 = 1002
-//			936 + 66 = 1002
-//			963 + 39 = 1002
-	
+		
 	
 	public boolean validateB(int B, int num) {
-		int k;
-	    int tmp = 0;
-	    int i = 0;
+//		int k;
+//	    int tmp = 0;
+//	    int i = 0;
 //        System.out.println("N:: "+num);
 //        System.out.println("B:: "+B);
 //        System.out.println("---------------");
-        
-        while(B > 0) {
-	        tmp = B % 10;
-//	        System.out.println("right most of B:: "+tmp);
-	        int N = num;
-			while(N > 0) {
-			  k=N%10;
-//		        System.out.println("right most of N:: "+k);
-			  if(k==tmp) {
-//				  i++;
-				  break;
-			  }
-			  N /= 10;
-//			  System.out.println("N:: "+ N);
+	    
+		if (countDigit(B) == 1 && B > 0) {
+			int k;
+			int leadingZeros = countDigit(num)-1;				
+			String formatted = String.format("%0"+leadingZeros+"d", B);
 
-			  if (k != B && N == 0) {
-				  return false;
-			  }
+			for (int j=0; j<formatted.length(); j++) {
+				int temp = Integer.parseInt(formatted.charAt(j)+"");
+		        int N = num;
+		        
+				while(N > 0) {
+				  k=N%10;
+				  if(k==temp) {
+					  break;
+				  }
+				  N /= 10;
+
+				  if (k != temp && N == 0) {
+					  return false;
+				  }
+				}				
 			}
-	        B /= 10;	        
-//	        System.out.println("B:: "+ B);
+	    } else {
+
+	    	int tmp = 0;
+		
+	        while(B > 0) {
+	    		int k;
+	
+	        	if (tmp == (B % 10)) {
+	        		return false;
+	        	}
+		        tmp = B % 10;
+	//	        System.out.println("right most of B:: "+tmp);
+		        int N = num;
+				while(N > 0) {
+				  k=N%10;
+	//		        System.out.println("right most of N:: "+k);
+				  if(k==tmp) {
+					  break;
+				  }
+				  N /= 10;
+	//			  System.out.println("N:: "+ N);
+	
+				  if (k != B && N == 0) {
+					  return false;
+				  }
+				}
+		        B /= 10;	        
+	//	        System.out.println("B:: "+ B);
+		    }
+	        
 	        
 	    }
+	        
 		return true;
 	}
 
@@ -69,14 +90,6 @@ public class Solution {
         } 
         return count; 
 	}
-	void addSquaresOfDigits(int number) {
-	    int tmp = 0;
-	    while(number > 0) {
-	        tmp = number % 10;
-	        System.out.println(tmp);
-	        number /= 10;
-	    }
-	}	
 	
 	
 }
